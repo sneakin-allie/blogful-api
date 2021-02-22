a11be80ba085316aaebd1d2bdbe44ea47c637372
Allison Schulman <allison.d.schulman@gmail.com>
2021-02-22 11:36:10 -0500

    Commit with POST and DELETE

--- src/app.js
+++ src/app.js
@@ -4,30 +4,31 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const articlesRouter = require('./articles/articles-router')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 app.use(cors())
+app.use(helmet())
 
-app.get('/', (req, rest) => {
-    rest.send('Hello, world!')
+app.use('/articles', articlesRouter)
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' }}
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: 'Server error' }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
 })
 
 module.exports = app
